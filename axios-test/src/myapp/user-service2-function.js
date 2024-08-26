eeb0409fa8ea40eb34fb72acb817131971827f3b
hothoony <hothoony@hanmail.net>
2024-08-26 16:52:18 +0900

    .

--- axios-test/src/myapp/user-service2-function.js
+++ axios-test/src/myapp/user-service2-function.js
@@ -6,29 +6,25 @@ const axiosInstance = axios.create({
     headers: {'X-Custom-Header': 'foobat'},
 });
 
-const UserService = () => {
-
-    const getUserList = async () => {
-        try {
-            // console.log('getUserList');
-            const result = await axiosInstance.get('/users/search?q=John');
-            // console.log('result', result);
-            // console.log('result status', result.status, result.statusText);
-            if (result.status !== 200) {
-                throw new Error(`${result.status} ${result.statusText}`);
-            }
-            // console.log('result.data', result.data);
-            return result.data;
-        } catch (err) {
-            console.error('err', err);
-            throw err;
+const getUserList = async () => {
+    try {
+        // console.log('getUserList');
+        const result = await axiosInstance.get('/users/search?q=John');
+        // console.log('result', result);
+        // console.log('result status', result.status, result.statusText);
+        if (result.status !== 200) {
+            throw new Error(`${result.status} ${result.statusText}`);
         }
-    };
+        // console.log('result.data', result.data);
+        return result.data;
+    } catch (err) {
+        console.error('err', err);
+        throw err;
+    }
+};
 
-    return (
-        getUserList
-    );
-
+const userService = {
+    getUserList,
 }
 
-export default UserService();
+export default userService;
