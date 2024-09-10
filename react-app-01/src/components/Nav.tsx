987e9e8c252a94cfb2dbc2acfaf231d6f9186cdc
hothoony <hothoony@hanmail.net>
2024-09-10 11:41:51 +0900

    .

--- react-app-01/src/components/Nav.tsx
+++ react-app-01/src/components/Nav.tsx
@@ -1,14 +1,11 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Nav() {
 
-    const nav1Display = 'none';
-    const nav2Display = 'block';
-
     return (
         <div>
-            Nav
-            <div style={{display: nav1Display}}>
+            
+            <div>
                 <h3>Menu - Link 사용</h3>
                 <ul>
                 <li><Link to="/">Home</Link></li>
@@ -19,15 +16,6 @@ export default function Nav() {
                 </ul>
             </div>
 
-            <div style={{display: nav2Display}}>
-                <h3>Menu - NavLink 사용</h3>
-                <ul>
-                <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink to="/about">About</NavLink></li>
-                <li><NavLink to="/contact">Contact</NavLink></li>
-                <li><NavLink to="/error404">404</NavLink></li>
-                </ul>
-            </div>
         </div>
     );
 }
